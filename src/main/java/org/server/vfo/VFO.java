f0eec094693e96a0068d75acdc13f8dfc69b4df1
backdevhong <ghddls0704@kakao.com>
2023-08-30 14:08:24 +0900

    init project

---
+++ src/main/java/org/server/vfo/VFO.java
@@ -0,0 +1,17 @@
+package org.server.vfo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VFO extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
